5f05e6bfd54e832fd1509630044fdabbf9e8bfec
Erin Stalzer <erinstalzer@gmail.com>
2019-07-15 14:28:15 -0400

    add code to sessions controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,15 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+  def create 
+    if params["name"] && params["name"] != ""
+      session[:name] = params["name"]
+    else
+      redirect_to '/sessions/new'
+    end
   end
 
   def destroy
+    session[.delete :name
   end
 end
