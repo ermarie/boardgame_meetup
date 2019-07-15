0a9935d3c7474ab920815fa27aabfaf049920e96
Erin Stalzer <erinstalzer@gmail.com>
2019-07-15 14:46:06 -0400

    fix sessions controller destroy code, add routes for models

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,6 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[.delete :name
+    session.delete :name
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
     get 'signup', to: 'devise/registrations#new'
   end
 
-  
+  resource :groups, :events, :games, :categories, :mechanisms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
