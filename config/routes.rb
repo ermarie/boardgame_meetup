d5626ef75aa69f8c7190f480d23cc0eba86ee23d
Erin Stalzer <erinstalzer@gmail.com>
2019-07-16 09:20:43 -0400

    add homepage and styling

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   end
 
   resource :groups, :events, :games, :categories, :mechanisms
+
+  root to: "sessions#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
