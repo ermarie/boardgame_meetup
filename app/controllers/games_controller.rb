d0de18a5742d3ff58416a2c6c65ebadd245e01a8
Erin Stalzer <erinstalzer@gmail.com>
2019-07-16 14:43:20 -0400

    add basic code for games controller and view page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,22 @@
 class GamesController < ApplicationController
   def new
+    @game = Game.new
   end
 
   def create
   end
 
   def index
+    @user = User.find_by(name: session[:name])
+    @games = @user.games
   end
 
   def show
+    @game = Game.find_by(id: params[:id])
   end
 
   def edit
+    @game = Game.find_by(id: params[:id])
   end
 
   def update
