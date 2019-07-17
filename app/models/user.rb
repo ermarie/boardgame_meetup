8d51691c8b2006e1c5d649ccd8456dd1202800bf
Erin Stalzer <erinstalzer@gmail.com>
2019-07-17 09:59:21 -0400

    fix signin error

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable, :omniauthable
 
     def self.from_omniauth(auth)
-      where(provider: auth.provider, uid:auth.id).first_or_create do |user|
+      where(provider: auth.provider, uid:auth.uid).first_or_create do |user|
         user.provider = auth.provider
         user.name = auth.info.name
         user.uid = auth.uid
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,14 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
-    #get 'logout', to: 'sessions#destroy'
   end
 
-  resources :groups do 
-    resources :events
-  end
+  # resources :groups do 
+  #   resources :events
+  # end
     
     
-  resources :events, :games, :categories, :mechanisms
+  #resources :events, :games, :categories, :mechanisms
 
   root to: "application#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
