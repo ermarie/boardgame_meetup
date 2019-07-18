cb3c06538dded74b8dab1b3ffbefd139e3a06ea1
Erin Stalzer <erinstalzer@gmail.com>
2019-07-18 14:45:05 -0400

    add scope methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,17 +2,8 @@ class Event < ApplicationRecord
     belongs_to :group
     has_many :users, through: :group
 
-    #scope :within_1_day, => {where: date_time < (Date.today + 1)}
+    scope :within_1_day, -> {where("date_time.to_date < ?", Date.today + 2)}
+    scope :within_7_days, -> {where("date_time.to_date < ?", Date.today + 8)}
+    scope :within_30_days, -> {where("date_time.to_date < ?", Date.today + 31)}
 
-    def within_1_day
-        Range.new(Date.today, Date.today + 1).include?(date_time.to_date)
-    end
-
-    def within_7_days
-        Range.new(Date.today, Date.today + 7).include?(date_time.to_date)
-    end
-
-    def witin_30_days
-        Range.new(Date.today, Date.today + 30).include?(date_time.to_date)
-    end
 end
