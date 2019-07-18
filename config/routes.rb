76cbba4ee48279cff6c0accfcb05a3de0bb56631
Erin Stalzer <erinstalzer@gmail.com>
2019-07-18 13:27:38 -0400

    create categories controller and make show action and page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'categories/show'
   devise_for :users, :controllers => { :registrations => "registrations", :omniauth_callbacks => "callbacks" }
   
   devise_scope :user do
