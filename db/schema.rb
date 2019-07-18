2842d39ff4127833ced9a0656782cedf13527af1
Erin Stalzer <erinstalzer@gmail.com>
2019-07-18 09:23:25 -0400

    create join table for users and games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_185638) do
+ActiveRecord::Schema.define(version: 2019_07_18_132246) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 2019_07_17_185638) do
     t.integer "max_age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "games_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "game_id", null: false
   end
 
   create_table "groups", force: :cascade do |t|
