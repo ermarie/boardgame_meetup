ad97ed04f02ebede0e80ed2da0e970d9c7127d66
Erin Stalzer <erinstalzer@gmail.com>
2019-07-19 11:37:51 -0400

    add delete button to game show page, set up scope methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
     belongs_to :group
     has_many :users, through: :group
 
+    scope :within_1_day, -> {where("date_time < ? and date_time > ?", Date.today + 2, Date.today)}
+    scope :within_7_days, -> {where("date_time < ? and date_time > ?", Date.today + 8, Date.today)}
+    scope :within_30_days, -> {where("date_time < ? and date_time > ?", Date.today + 31, Date.today)}
+
 end
