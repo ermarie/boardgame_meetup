d1153be593519f0b3c60fc389f63627ef766b6e5
Erin Stalzer <erinstalzer@gmail.com>
2019-07-22 15:49:28 -0400

    remove unneccesary controllers

--- app/controllers/mechanisms_controller.rb
+++
@@ -1,9 +0,0 @@
-class MechanismsController < ApplicationController
-  def show
-    @mechanism = Mechanism.find_by_id(params[:id])
-  end
-
-  def index 
-    @mechanisms = Mechanism.all
-  end
-end
